eaf01934d0ed6d46b3123235acebec7a3b2cae23
satyamsinha111 <satyamsinha8158@gmail.com>
2022-04-09 00:01:11 +0530

    added item section

--- src/app/pages/dashboard/pages/items/items.component.ts
+++ src/app/pages/dashboard/pages/items/items.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.scss']
+  styleUrls: ['./items.component.scss'],
 })
 export class ItemsComponent implements OnInit {
+  categoryList: any[] = [
+    {
+      category_id: 1,
+      category_name: 'Lunch',
+      category_desc: 'lorem ipsum',
+    },
+    {
+      category_id: 1,
 
-  constructor() { }
+      category_name: 'Breakfast',
+      category_desc: 'lorem ipsum',
+    },
+    {
+      category_id: 1,
+
+      category_name: 'Dinner',
+      category_desc: 'lorem ipsum',
+    },
+  ];
+  dataSource: any[] = [];
+  displayedColumns: any[] = ['category_id', 'category_name', 'category_desc'];
+  constructor() {}
 
   ngOnInit(): void {
+    this.dataSource = this.categoryList;
   }
-
 }
--- src/app/pages/dashboard/pages/items/items.module.ts
+++ src/app/pages/dashboard/pages/items/items.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ItemsRoutingModule } from './items-routing.module';
 import { ItemsComponent } from './items.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    ItemsComponent
-  ],
+  declarations: [ItemsComponent],
   imports: [
     CommonModule,
-    ItemsRoutingModule
-  ]
+    ItemsRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatTableModule,
+  ],
 })
-export class ItemsModule { }
+export class ItemsModule {}
